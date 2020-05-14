90c8e90a8fd223755b47d8f465850f0d3f9b5931
Mirmillon <xavier.compagnion@gmail.com>
2020-05-14 13:28:23 +0800

    Premier commit

---
+++ Pages/CountriesListBase.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using PeopleManagement.Models.Telecommunications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleManagement.Web.Pages
+{
+    public class CountriesListBase : ComponentBase
+    {
+      
+        public List<Country> Countries { get; set; }
+
+        public CountriesListBase()
+        {
+            LoadCountries();
+        }
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadCountries();
+            return base.OnInitializedAsync();
+        }
+
+        private void LoadCountries()
+        {
+            Country c1 = new Country
+            {
+                NameCountry= "Philippines",
+                CodeISO156A2="PH",
+                TelephonePrefix ="63"
+
+
+            };
+
+            Country c2 = new Country
+            {
+                NameCountry = "France",
+                CodeISO156A2 = "FR",
+                TelephonePrefix = "33"
+            };
+
+            Country c3 = new Country
+            {
+                NameCountry = "Belgique",
+                CodeISO156A2 = "BE",
+                TelephonePrefix = "32"
+            };
+
+
+            Countries = new List<Country> { c3, c2, c1 };
+        }
+        
+    }
+}
---
+++ Pages/PersonsListBase.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Components;
+using PeopleManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleManagement.Web.Pages
+{
+    public class PersonsListBase : ComponentBase
+    {
+        readonly CountriesListBase List = new CountriesListBase();
+
+        public IEnumerable<Person> Persons { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await Task.Run(LoadPersons);
+          //  return base.OnInitializedAsync();
+        }
+
+        private void LoadPersons()
+        {
+            System.Threading.Thread.Sleep(3000);
+            Person p1 = new Person
+           {
+                PersonId = 1,
+                FirstName = "Rose-Ann",
+                MiddleName = "Masayo",
+                LastName = "Alecto",
+                DateOfBirth = new DateTime ( 1991, 11, 12 ),
+                Alive = 'Y',
+                Gender = Gender.Female,
+                PicturePath = "images/mary.png",
+                MobileId = 1
+            };
+
+            Person p2 = new Person {
+                PersonId = 2,
+                FirstName = "  Xavier    Robert Gérard",
+                MiddleName = null,
+                LastName = "Compagnion",
+                DateOfBirth = new DateTime(1954, 01, 08),
+                Alive = 'Y',
+                Gender = Gender.Male,
+                PicturePath = "images/Moi.jpg",
+                MobileId = 2
+            };
+
+            Person p3 = new Person {
+                PersonId = 3,
+                FirstName = "Christian",
+                MiddleName = null,
+                LastName = "Compagnion",
+                DateOfBirth = new DateTime(1946, 04, 13),
+                Alive = 'Y',
+                Gender = Gender.Male,
+                PicturePath = "images/sam.jpg",
+                MobileId = 3
+            };
+            Person p4 = new Person {
+                PersonId = 4,
+                FirstName = "Sabine Marie Ghislaine  ",
+                MiddleName = null,
+                LastName = "Compagnion",
+                DateOfBirth = new DateTime(1951, 11, 05),
+                Alive = 'Y',
+                Gender = Gender.Female,
+                PicturePath = "images/sara.png",
+                MobileId = 4
+            };
+
+            Persons = new List<Person> { p1, p2, p3, p4 };
+        }
+    }
+}
